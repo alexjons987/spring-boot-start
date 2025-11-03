3e089f3b742d23848c89f7ac4a5a6337f8795953
alexjons987 <alexander.jonsson987@outlook.com>
2025-11-03 10:29:42 +0100

    Add excercise 2

---
+++ hello-spring/src/main/java/com/example/hello_spring/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.hello_spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello Spring Boot!";
+    }
+
+    @GetMapping("/bye")
+    public String sayBye() {
+        return "Goodbye Spring!";
+    }
+}
